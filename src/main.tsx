954b776d3015832414f72ef7f04c14759cd854ff
Tw Riviera Deployer <deploy@tw-riviera.github.io>
2026-05-26 02:24:05 +0800

    Add scroll to top on route change

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { HashRouter, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import './index.css'
 import App from './App.tsx'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 createRoot(document.getElementById('root')!).render(
   <HashRouter>
+    <ScrollToTop />
     <App />
   </HashRouter>,
 )
